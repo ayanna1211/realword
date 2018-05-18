f17628319c2d1fdea29d274f17d0e44dea4f4dfc
thao.nnm <thao.nnm@sutrixsolutions.com>
2018-05-18 17:33:53 +0700

    add required register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public username:string;
+  public email:string;
+  public password:string;
+  constructor() {
+    this.username = '';
+    this.email = '';
+    this.password = '';
+  }
 
   ngOnInit() {
   }
 
+  public submit() {
+    if (this.username !== '') {
+      console.log(this.username);
+    }
+
+    if (this.email !== '') {
+      console.log(this.email);
+    }
+
+    if (this.password !== '') {
+      console.log(this.password);
+    }
+  }
 }
